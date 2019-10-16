1b667608462ad7c527aaa7bcf84353213ffce042
Rimvydas Alminas <38453352+s032197@users.noreply.github.com>
2019-10-16 08:04:05 +0300

    Update main.rb

--- ruby-1-uzd/Trikampis/main.rb
+++ ruby-1-uzd/Trikampis/main.rb
@@ -5,12 +5,13 @@ put1 = gets.to_f
 put2 = gets.to_f
 put3 = gets.to_f
 
-def trikapmio_perimetras(put1, put2, put3)
+def trikapmio_pusperimetris(put1, put2, put3)
   p = (put1 + put2 + put3) / 2
   puts "Perimetras yra : #{p}"
   if put1 <= 0 || put2 <= 0 || put2 <= 0
     puts 'Negali buti neigiamu krastiniu'
-  elsif put1 + put2 > put3 && put2 + put3 > put1 && put1 + put3 > put2
+  else
+    put1 + put2 > put3 && put2 + put3 > put1 && put1 + put3 > put2
     puts 'Trikampis egzistuoja'
     plotas = Math.sqrt(p * (p - put1) * (p - put2) * (p - put3))
     puts "Trikampio plotas #{plotas}"
@@ -26,5 +27,5 @@ def trikampis(put1, put2, put3)
     puts 'Trikampis ivairiakrastis'
   end
 end
-puts trikapmio_perimetras(put1, put2, put3).to_s
+puts trikapmio_pusperimetris(put1, put2, put3).to_s
 puts trikampis(put1, put2, put3).to_s
