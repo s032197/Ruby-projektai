4dea7885ace366566fd399775ea67d750f00aadf
Rimvydas Alminas <38453352+s032197@users.noreply.github.com>
2019-10-23 08:59:06 +0300

    Update main.rb

--- ruby-1-uzd/Trikampis/main.rb
+++ ruby-1-uzd/Trikampis/main.rb
@@ -1,31 +1,54 @@
 # frozen_string_literal: true
 
+# Description/Explanation of Trikampis class
+class Trikampis
+  attr_accessor :trikmapio_tipas
+  def initialize(put1, put2, put3)
+    @put1 = put1
+    @put2 = put2
+    @put3 = put3
+    @trikmapio_tipas = triangle
+  end
+
+  def pusperimetris
+    (@put1 + @put2 + @put3) / 2
+  end
+
+  def trikampio_plotas(pusperimetris)
+    Math.sqrt(
+      pusperimetris * (pusperimetris - @put1) *
+      (pusperimetris - @put2) * (pusperimetris - @put3)
+    )
+  end
+
+  def triangle
+    return 'Trikampis lygiakrastis' if lygiakrastis?
+    return 'Trikampis lygiasonis' if lygiasonis?
+    return 'Ivairekrastis' if ivairiakrastis?
+
+    'Trinkampis neimanomas'
+  end
+
+  def lygiakrastis?
+    @put1 == @put2 && @put2 == @put3 && @put3 == @put1
+  end
+
+  def lygiasonis?
+    @put1 == @put2 || @put2 == @put3 || @put1 == @put3
+  end
+
+  def ivairiakrastis?
+    @put1 != @put2 && @put2 != @put3 && @put1 != @put3
+  end
+end
+
 puts 'Ivesk krastiniu ilgius'
 put1 = gets.to_f
 put2 = gets.to_f
 put3 = gets.to_f
 
-def trikapmio_pusperimetris(put1, put2, put3)
-  p = (put1 + put2 + put3) / 2
-  puts "Perimetras yra : #{p}"
-  if put1 <= 0 || put2 <= 0 || put2 <= 0
-    puts 'Negali buti neigiamu krastiniu'
-  else
-    put1 + put2 > put3 && put2 + put3 > put1 && put1 + put3 > put2
-    puts 'Trikampis egzistuoja'
-    plotas = Math.sqrt(p * (p - put1) * (p - put2) * (p - put3))
-    puts "Trikampio plotas #{plotas}"
-  end
-end
-
-def trikampis(put1, put2, put3)
-  if put1 == put2 && put2 == put3 && put3 == put1
-    puts 'Trikampis lygiakrastis'
-  elsif put1 == put2 || put2 == put3 || put1 == put3
-    puts 'Trikampis lygiasonis'
-  elsif put1 != put2 && put2 != put3 && put1 != put3
-    puts 'Trikampis ivairiakrastis'
-  end
-end
-puts trikapmio_pusperimetris(put1, put2, put3).to_s
-puts trikampis(put1, put2, put3).to_s
+trikampis = Trikampis.new(put1, put2, put3)
+trikampis.pusperimetris
+puts "Trikampio pusperimetris yra : #{trikampis.pusperimetris}"
+trikampis.trikampio_plotas(trikampis.pusperimetris)
+puts "Plotas : #{trikampis.trikampio_plotas(trikampis.pusperimetris)}"
